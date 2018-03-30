f599a3e4d7c2182847998b60269e2e3b1933b9a5
Alexander Alkhimyonok <buckartz@gmail.com>
2018-03-30 23:34:19 +0300

    project initialization

---
+++ src/main/java/com/gmail/buckartz/roomreservation/RoomReservationApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.buckartz.roomreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationApplication.class, args);
+	}
+}
