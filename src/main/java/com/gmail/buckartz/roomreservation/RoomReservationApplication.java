afdc6f7940fd2d82ecb537ce526493938d8be194
Alexander Alkhimyonok <buckartz@gmail.com>
2018-03-31 11:43:56 +0300

    RestApi developing for room creation - dev and test profile creation

--- src/main/java/com/gmail/buckartz/roomreservation/RoomReservationApplication.java
+++ src/main/java/com/gmail/buckartz/roomreservation/RoomReservationApplication.java
@@ -2,11 +2,15 @@ package com.gmail.buckartz.roomreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile({"dev", "test"})
 public class RoomReservationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoomReservationApplication.class, args);
+		SpringApplication app = new SpringApplication(RoomReservationApplication.class);
+		app.setAdditionalProfiles("dev");
+		app.run(args);
 	}
 }
