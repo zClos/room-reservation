46b62877457b2520e39c65d92a4e1e12fc64e906
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-02 17:49:14 +0300

    Rest API developing for employee creation - Employee service creation

---
+++ src/main/java/com/gmail/buckartz/roomreservation/service/employee/EmployeeSaveService.java
@@ -0,0 +1,7 @@
+package com.gmail.buckartz.roomreservation.service.employee;
+
+import com.gmail.buckartz.roomreservation.domain.Employee;
+import com.gmail.buckartz.roomreservation.service.SaveService;
+
+public interface EmployeeSaveService extends SaveService<Employee> {
+}
---
+++ src/main/java/com/gmail/buckartz/roomreservation/service/employee/impl/EmployeeSaveServiceImpl.java
@@ -0,0 +1,18 @@
+package com.gmail.buckartz.roomreservation.service.employee.impl;
+
+import com.gmail.buckartz.roomreservation.dao.employee.EmployeeSaveDao;
+import com.gmail.buckartz.roomreservation.domain.Employee;
+import com.gmail.buckartz.roomreservation.service.employee.EmployeeSaveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeSaveServiceImpl implements EmployeeSaveService {
+    @Autowired
+    private EmployeeSaveDao employeeSaveDao;
+
+    @Override
+    public void save(Employee employee) {
+        employeeSaveDao.save(employee);
+    }
+}
