707b4750be337326c74dc575c3915002b9a93f3d
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-02 20:36:44 +0300

    Rest API developing for employee creation validation - Employee validation service testing

--- src/test/java/com/gmail/buckartz/roomreservation/controller/employee/EmployeeControllerTests.java
+++ src/test/java/com/gmail/buckartz/roomreservation/controller/employee/EmployeeControllerTests.java
@@ -11,9 +11,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IntegrationWebTestConfiguration
@@ -44,5 +46,45 @@ public class EmployeeControllerTests extends ControllerTestContext {
                                         fieldWithPath("firstName").description(""),
                                         fieldWithPath("lastName").description("")
                                 )));
+    }
+
+    @Test
+    public void saveEmployeeWithEmptyValue() throws Exception {
+        EmployeeMapper mapper = EmployeeMapper.builder()
+                .firstName("")
+                .lastName("")
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Charset", "utf-8");
+
+        getMockMvc().perform(post("/employee")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(headers)
+                .content(objectMapper.writeValueAsString(mapper)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.fieldsViolations.firstName", containsInAnyOrder("Employee first name must not be empty")))
+                .andExpect(jsonPath("$.fieldsViolations.lastName", containsInAnyOrder("Employee last name must not be empty")));
+    }
+
+    @Test
+    public void saveEmployeeWithBadLengthValues() throws Exception {
+        EmployeeMapper mapper = EmployeeMapper.builder()
+                .firstName("Aaaaaaaaaaddddddddasdds")
+                .lastName("Z")
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Charset", "utf-8");
+
+        getMockMvc().perform(post("/employee")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(headers)
+                .content(objectMapper.writeValueAsString(mapper)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.fieldsViolations.firstName", containsInAnyOrder("Size of employee first name must not be less than 2 and more than 16 characters")))
+                .andExpect(jsonPath("$.fieldsViolations.lastName", containsInAnyOrder("Size of employee last name must not be less than 2 and more than 16 characters")));
     }
 }
