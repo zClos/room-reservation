c0192fef878cac628c3a773269b9801291e4941f
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-03 04:05:52 +0300

    Rest API developing for room getting all - Controller creating

--- src/main/java/com/gmail/buckartz/roomreservation/controller/room/RoomController.java
+++ src/main/java/com/gmail/buckartz/roomreservation/controller/room/RoomController.java
@@ -1,18 +1,22 @@
 package com.gmail.buckartz.roomreservation.controller.room;
 
 import com.gmail.buckartz.roomreservation.controller.DefaultHeaderValues;
+import com.gmail.buckartz.roomreservation.domain.Room;
 import com.gmail.buckartz.roomreservation.mapping.room.RoomMapping;
 import com.gmail.buckartz.roomreservation.mapping.room.mapper.RoomMapper;
+import com.gmail.buckartz.roomreservation.service.room.RoomGetAllService;
 import com.gmail.buckartz.roomreservation.service.room.RoomSaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/room")
@@ -21,12 +25,22 @@ public class RoomController {
     @Autowired
     private RoomSaveService roomSaveService;
 
+
     @Autowired
     private RoomMapping roomMapping;
+
+    @Autowired
+    private RoomGetAllService getAllService;
 
     @PostMapping
     public ResponseEntity saveRoom(@Valid @RequestBody RoomMapper mapper) {
         roomSaveService.save(roomMapping.toObject(mapper));
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<Room>> getAllRooms() {
+        Set<Room> rooms = getAllService.findAll();
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
     }
 }
