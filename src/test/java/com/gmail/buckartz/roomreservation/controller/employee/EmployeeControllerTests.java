0e7ff7d23c68fa681d308f8dde032a5f6e6bdf80
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-05 14:56:46 +0300

    Rest API developing for room reservation creation validation - Time validation service testing

--- src/test/java/com/gmail/buckartz/roomreservation/controller/employee/EmployeeControllerTests.java
+++ src/test/java/com/gmail/buckartz/roomreservation/controller/employee/EmployeeControllerTests.java
@@ -6,8 +6,10 @@ import com.gmail.buckartz.roomreservation.config.IntegrationWebTestConfiguration
 import com.gmail.buckartz.roomreservation.domain.Employee;
 import com.gmail.buckartz.roomreservation.domain.Room;
 import com.gmail.buckartz.roomreservation.mapping.employee.mapper.EmployeeMapper;
+import com.gmail.buckartz.roomreservation.mapping.reservation.ReservationMapping;
 import com.gmail.buckartz.roomreservation.mapping.reservation.mapper.ReservationMapper;
 import com.gmail.buckartz.roomreservation.service.employee.EmployeeSaveService;
+import com.gmail.buckartz.roomreservation.service.reservation.ReservationSaveService;
 import com.gmail.buckartz.roomreservation.service.room.RoomSaveService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -36,6 +39,22 @@ public class EmployeeControllerTests extends ControllerTestContext {
 
     @Autowired
     private RoomSaveService roomSaveService;
+
+    @Autowired
+    private ReservationSaveService reservationSaveService;
+
+    @Autowired
+    private ReservationMapping reservationMapping;
+
+    private LocalDateTime getTestDateTime() {
+        return Optional.of(LocalDateTime.now()).map(now -> {
+            if (now.plusHours(2).getHour() > 18 || now.getHour() < 9) {
+                return LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth() + 1,
+                        12, 00);
+            }
+            return now.minusSeconds(now.getSecond()).minusNanos(now.getNano());
+        }).orElse(null);
+    }
 
     @Test
     public void saveEmployee() throws Exception {
@@ -115,9 +134,8 @@ public class EmployeeControllerTests extends ControllerTestContext {
                 .build();
         roomSaveService.save(room);
 
-        LocalDateTime dateTimeFrom = LocalDateTime.now();
-        dateTimeFrom = dateTimeFrom.minusSeconds(dateTimeFrom.getSecond()).minusNanos(dateTimeFrom.getNano());
-        LocalDateTime dateTimeTo = dateTimeFrom.minusMinutes(30);
+        LocalDateTime dateTimeFrom = getTestDateTime();
+        LocalDateTime dateTimeTo = dateTimeFrom.plusMinutes(30);
 
         ReservationMapper mapper = ReservationMapper.builder()
                 .roomId(room.getId())
@@ -146,7 +164,7 @@ public class EmployeeControllerTests extends ControllerTestContext {
     }
 
     @Test
-    public void saveEmployeeRoomReservationForNotExistOne() throws Exception {
+    public void saveEmployeeRoomReservationForNotExistEmployee() throws Exception {
         Employee employee = Employee.builder()
                 .firstName("Big")
                 .lastName("Boss")
@@ -159,9 +177,8 @@ public class EmployeeControllerTests extends ControllerTestContext {
                 .build();
         roomSaveService.save(room);
 
-        LocalDateTime dateTimeFrom = LocalDateTime.now();
-        dateTimeFrom = dateTimeFrom.minusSeconds(dateTimeFrom.getSecond()).minusNanos(dateTimeFrom.getNano());
-        LocalDateTime dateTimeTo = dateTimeFrom.minusMinutes(30);
+        LocalDateTime dateTimeFrom = getTestDateTime();
+        LocalDateTime dateTimeTo = dateTimeFrom.plusMinutes(30);
 
         ReservationMapper mapper = ReservationMapper.builder()
                 .roomId(room.getId())
@@ -196,9 +213,8 @@ public class EmployeeControllerTests extends ControllerTestContext {
                 .build();
         roomSaveService.save(room);
 
-        LocalDateTime dateTimeFrom = LocalDateTime.now();
-        dateTimeFrom = dateTimeFrom.minusSeconds(dateTimeFrom.getSecond()).minusNanos(dateTimeFrom.getNano());
-        LocalDateTime dateTimeTo = dateTimeFrom.minusMinutes(30);
+        LocalDateTime dateTimeFrom = getTestDateTime();
+        LocalDateTime dateTimeTo = dateTimeFrom.plusMinutes(30);
 
         ReservationMapper mapper = ReservationMapper.builder()
                 .roomId(room.getId() + 1)
@@ -218,5 +234,148 @@ public class EmployeeControllerTests extends ControllerTestContext {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.fieldsViolations.roomId",
                         containsInAnyOrder("Room with id " + (room.getId() + 1) + " doesn't exist")));
+    }
+
+    @Test
+    public void saveEmployeeRoomReservationEmptyValues() throws Exception {
+        Employee employee = Employee.builder()
+                .firstName("Big")
+                .lastName("Boss")
+                .build();
+        employeeSaveService.save(employee);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 500; ) {
+            stringBuilder.append(++i);
+        }
+
+        ReservationMapper mapper = ReservationMapper.builder()
+                .roomId(null)
+                .reservedFrom("")
+                .reservedTo("")
+                .reason(stringBuilder.toString())
+                .builder();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Charset", "utf-8");
+
+        getMockMvc().perform(post("/employee/{id}/reservation", employee.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(headers)
+                .content(objectMapper.writeValueAsString(mapper)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.fieldsViolations.roomId", containsInAnyOrder("Room id can't be empty")))
+                .andExpect(jsonPath("$.fieldsViolations.reservedFrom", containsInAnyOrder("Field \'reservedFrom\' can't be empty")))
+                .andExpect(jsonPath("$.fieldsViolations.reservedTo", containsInAnyOrder("Field \'reservedTo\' can't be empty")))
+                .andExpect(jsonPath("$.fieldsViolations.reason", containsInAnyOrder("Text field shouldn't be more than 500 characters")));
+    }
+
+    @Test
+    public void saveEmployeeRoomReservationCorrectValuesGroup() throws Exception {
+        Employee employee = Employee.builder()
+                .firstName("Big")
+                .lastName("Boss")
+                .build();
+        employeeSaveService.save(employee);
+
+        ReservationMapper mapper = ReservationMapper.builder()
+                .roomId(25L)
+                .reservedFrom("2018-02-31R18:42")
+                .reservedTo("2018-20-35R12:42")
+                .reason("")
+                .builder();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Charset", "utf-8");
+
+        getMockMvc().perform(post("/employee/{id}/reservation", employee.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(headers)
+                .content(objectMapper.writeValueAsString(mapper)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.fieldsViolations.roomId", containsInAnyOrder("Room with id 25 doesn't exist")))
+                .andExpect(jsonPath("$.fieldsViolations.reservedFrom", containsInAnyOrder("Invalid format for 2018-02-31R18:42, follow the ISO template - YYYY-MM-DD{T}HH:MM but drop braces")))
+                .andExpect(jsonPath("$.fieldsViolations.reservedTo", containsInAnyOrder("Invalid format for 2018-20-35R12:42, follow the ISO template - YYYY-MM-DD{T}HH:MM but drop braces")));
+    }
+
+    @Test
+    public void saveEmployeeRoomReservationOutOfWorkTime() throws Exception {
+        Employee employee = Employee.builder()
+                .firstName("Big")
+                .lastName("Boss")
+                .build();
+        employeeSaveService.save(employee);
+
+        Room room = Room.builder()
+                .number("11")
+                .sitsCount(5)
+                .build();
+        roomSaveService.save(room);
+
+        ReservationMapper mapper = ReservationMapper.builder()
+                .roomId(room.getId())
+                .reservedFrom("2018-04-07T10:00")
+                .reservedTo("2018-04-06T18:01")
+                .reason("")
+                .builder();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Charset", "utf-8");
+
+        getMockMvc().perform(post("/employee/{id}/reservation", employee.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(headers)
+                .content(objectMapper.writeValueAsString(mapper)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.fieldsViolations.reservedFrom",
+                        containsInAnyOrder("Bad request time 2018-04-07T10:00 - company works from 09:00 to 18:00 everyday except [SATURDAY, SUNDAY]")))
+                .andExpect(jsonPath("$.fieldsViolations.reservedTo",
+                        containsInAnyOrder("Bad request time 2018-04-06T18:01 - company works from 09:00 to 18:00 everyday except [SATURDAY, SUNDAY]")));
+    }
+
+    @Test
+    public void saveEmployeeRoomReservationInExistPeriod() throws Exception {
+        Employee employee = Employee.builder()
+                .firstName("Big")
+                .lastName("Boss")
+                .build();
+        employeeSaveService.save(employee);
+
+        Room room = Room.builder()
+                .number("11")
+                .sitsCount(5)
+                .build();
+        roomSaveService.save(room);
+
+        LocalDateTime dateTime = getTestDateTime();
+
+        ReservationMapper mapper = ReservationMapper.builder()
+                .roomId(room.getId())
+                .reservedFrom(dateTime.toString())
+                .reservedTo(dateTime.plusHours(1).toString())
+                .reason("")
+                .employeeId(employee.getId())
+                .builder();
+        reservationSaveService.save(reservationMapping.toObject(mapper));
+
+        mapper = ReservationMapper.builder(mapper)
+                .reservedFrom(dateTime.plusMinutes(15).toString())
+                .reservedTo(dateTime.plusHours(2).toString())
+                .builder();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Charset", "utf-8");
+
+        getMockMvc().perform(post("/employee/{id}/reservation", employee.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(headers)
+                .content(objectMapper.writeValueAsString(mapper)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.commonViolations",
+                        containsInAnyOrder("Room has already reserved for that period, or period is bad")));
     }
 }
--- src/test/java/com/gmail/buckartz/roomreservation/service/reservation/ReservationServiceTest.java
+++ src/test/java/com/gmail/buckartz/roomreservation/service/reservation/ReservationServiceTest.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @UnitTestConfiguration
@@ -24,6 +25,9 @@ public class ReservationServiceTest {
 
     @Autowired
     private RoomSaveService roomSaveService;
+    @Autowired
+    private ReservationGetCountByRoomAndPeriodService byRoomAndPeriodService;
+
 
     @Test
     public void saveReservation() {
@@ -46,5 +50,42 @@ public class ReservationServiceTest {
         assertNotNull(reservation.getId());
         assertNotNull(reservation.getRoom().getId());
         assertNotNull(reservation.getEmployee().getId());
+    }
+
+    @Test
+    public void getCountReservationInPeriod() {
+        Room room = Room.builder()
+                .number("1111")
+                .build();
+        roomSaveService.save(room);
+        Reservation reservation1 = Reservation.builder()
+                .room(room)
+                .employee(Employee.builder()
+                        .firstName("Ivan")
+                        .lastName("Ivanov")
+                        .build())
+                .reservedFrom(Timestamp.valueOf(LocalDateTime.parse("2018-04-04T12:52", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reservedTo(Timestamp.valueOf(LocalDateTime.parse("2018-04-04T13:50", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reason("Dance")
+                .build();
+        reservationSaveService.save(reservation1);
+        Reservation reservation2 = Reservation.builder()
+                .room(room)
+                .employee(Employee.builder()
+                        .firstName("Ivan1")
+                        .lastName("Ivanov1")
+                        .build())
+                .reservedFrom(Timestamp.valueOf(LocalDateTime.parse("2018-04-04T11:52", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reservedTo(Timestamp.valueOf(LocalDateTime.parse("2018-05-04T14:50", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reason("Dance1")
+                .build();
+        reservationSaveService.save(reservation2);
+
+        int aLong = byRoomAndPeriodService.execute(room.getId(),
+                Timestamp.valueOf(LocalDateTime.parse("2018-04-04T12:55", DateTimeFormatter.ISO_LOCAL_DATE_TIME)),
+                Timestamp.valueOf(LocalDateTime.parse("2018-04-04T13:31", DateTimeFormatter.ISO_LOCAL_DATE_TIME)));
+
+        System.out.println(aLong);
+        assertEquals(2, aLong);
     }
 }
