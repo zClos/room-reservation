9c42f15f8d8c8342157254a00885caa1925f8148
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-05 18:42:11 +0300

    Rest API developing for employee's reservations getting - Service testing

--- src/test/java/com/gmail/buckartz/roomreservation/service/reservation/ReservationServiceTest.java
+++ src/test/java/com/gmail/buckartz/roomreservation/service/reservation/ReservationServiceTest.java
@@ -4,6 +4,7 @@ import com.gmail.buckartz.roomreservation.config.UnitTestConfiguration;
 import com.gmail.buckartz.roomreservation.domain.Employee;
 import com.gmail.buckartz.roomreservation.domain.Reservation;
 import com.gmail.buckartz.roomreservation.domain.Room;
+import com.gmail.buckartz.roomreservation.service.employee.EmployeeSaveService;
 import com.gmail.buckartz.roomreservation.service.room.RoomSaveService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,9 +14,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @UnitTestConfiguration
 @RunWith(SpringRunner.class)
@@ -28,6 +31,11 @@ public class ReservationServiceTest {
     @Autowired
     private ReservationGetCountByRoomAndPeriodService byRoomAndPeriodService;
 
+    @Autowired
+    private EmployeeSaveService employeeSaveService;
+
+    @Autowired
+    private ReservationGetAllByEmployeeIdService allByEmployeeIdService;
 
     @Test
     public void saveReservation() {
@@ -87,5 +95,42 @@ public class ReservationServiceTest {
 
         System.out.println(aLong);
         assertEquals(2, aLong);
+    }
+
+    @Test
+    public void getAllReservationByEmployee() {
+        Employee employee = Employee.builder()
+                .firstName("Alen")
+                .lastName("Delon")
+                .build();
+        employeeSaveService.save(employee);
+
+        Reservation reservation1 = Reservation.builder()
+                .room(Room.builder()
+                        .number("12")
+                        .build())
+                .employee(employee)
+                .reservedFrom(Timestamp.valueOf(LocalDateTime.parse("2018-04-04T12:52", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reservedTo(Timestamp.valueOf(LocalDateTime.parse("2018-04-04T13:50", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reason("Dance")
+                .build();
+        reservationSaveService.save(reservation1);
+
+        Reservation reservation2 = Reservation.builder()
+                .room(Room.builder()
+                        .number("13")
+                        .build())
+                .employee(employee)
+                .reservedFrom(Timestamp.valueOf(LocalDateTime.parse("2018-04-04T11:52", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reservedTo(Timestamp.valueOf(LocalDateTime.parse("2018-05-04T14:50", DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .reason("Dance Too")
+                .build();
+        reservationSaveService.save(reservation2);
+
+        List<Reservation> reservationList = allByEmployeeIdService.getAllByParentId(employee.getId());
+
+        assertEquals(2, reservationList.size());
+        assertTrue(reservationList.contains(reservation1));
+        assertTrue(reservationList.contains(reservation2));
     }
 }
