814c1d4d3234db982ca430c5c98752e8c7eb8507
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-06 23:54:27 +0300

    Rest API developing for employee's reservations getting in period - Controller altering

--- src/main/java/com/gmail/buckartz/roomreservation/controller/employee/EmployeeController.java
+++ src/main/java/com/gmail/buckartz/roomreservation/controller/employee/EmployeeController.java
@@ -8,8 +8,9 @@ import com.gmail.buckartz.roomreservation.mapping.reservation.ReservationMapping
 import com.gmail.buckartz.roomreservation.mapping.reservation.ReservationToJsonListMapping;
 import com.gmail.buckartz.roomreservation.mapping.reservation.mapper.ReservationMapper;
 import com.gmail.buckartz.roomreservation.service.employee.EmployeeSaveService;
-import com.gmail.buckartz.roomreservation.service.reservation.ReservationGetAllByEmployeeIdService;
+import com.gmail.buckartz.roomreservation.service.reservation.ReservationGetAllByFilterService;
 import com.gmail.buckartz.roomreservation.service.reservation.ReservationSaveService;
+import com.gmail.buckartz.roomreservation.service.reservation.filter.ReservationSearchFilters;
 import com.gmail.buckartz.roomreservation.validation.employee.EmployeeIdExistenceConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,9 +22,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @DefaultHeaderValues
@@ -43,10 +46,10 @@ public class EmployeeController {
     private ReservationMapping reservationMapping;
 
     @Autowired
-    private ReservationGetAllByEmployeeIdService getAllByEmployeeIdService;
+    private ReservationToJsonListMapping reservationToJsonListMapping;
 
     @Autowired
-    private ReservationToJsonListMapping reservationToJsonListMapping;
+    private ReservationGetAllByFilterService getAllByFilterService;
 
     @PostMapping
     public ResponseEntity saveEmployee(@Valid @RequestBody EmployeeMapper employeeMapper) {
@@ -64,8 +67,17 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}/reservation")
-    public ResponseEntity<List<Reservation>> getAllEmployeeRoomReservations(@EmployeeIdExistenceConstraint @PathVariable("id") Long id) {
-        List<Reservation> list = getAllByEmployeeIdService.getAllByParentId(id);
+    public ResponseEntity<List<Reservation>> getAllEmployeeRoomReservations(@EmployeeIdExistenceConstraint @PathVariable("id") Long id,
+                                                                            @RequestParam("from") Optional<String> from,
+                                                                            @RequestParam("to") Optional<String> to,
+                                                                            @RequestParam("order") Optional<String> order) {
+        ReservationSearchFilters filters = ReservationSearchFilters.builder()
+                .id(id)
+                .from(from.orElse(null))
+                .to(to.orElse(null))
+                .order(order.orElse(null))
+                .build();
+        List<Reservation> list = getAllByFilterService.getAllByFilter(filters);
         return new ResponseEntity(reservationToJsonListMapping.toJson(list), HttpStatus.OK);
     }
 }
