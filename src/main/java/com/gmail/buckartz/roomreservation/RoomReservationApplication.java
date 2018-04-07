8f92ce96c9cbd1624083586d27ca5392ef75393d
Alexander Alkhimyonok <buckartz@gmail.com>
2018-04-07 21:38:00 +0300

    Deploy to existing servlet container - Override SpringBootServletInitializer configure method

--- src/main/java/com/gmail/buckartz/roomreservation/RoomReservationApplication.java
+++ src/main/java/com/gmail/buckartz/roomreservation/RoomReservationApplication.java
@@ -2,15 +2,22 @@ package com.gmail.buckartz.roomreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Profile({"dev", "test"})
-public class RoomReservationApplication {
+public class RoomReservationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(RoomReservationApplication.class);
 		app.setAdditionalProfiles("dev");
 		app.run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RoomReservationApplication.class);
 	}
 }
